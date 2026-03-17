c98865a361239a41d9fc7789422e04272ac6220c
EsterCarrion <carrionester2005@gmail.com>
2026-03-17 08:55:17 -0500

    Agregar archivos de proyecto.

---
+++ Biblioteca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaModelos
+{
+    public class Biblioteca
+    {
+        //Llave primaria
+        [Key] public int Id { get; set; }
+
+        public string Nombre_Biblioteca { get; set; }
+        public string Dirección { get; set; }
+
+
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LibreriaModelos
+{
+    public class Class1
+    {
+
+    }
+}
